9cd95975a685233bc01d7773894bb16d95559d4b
Kazunori Akiyama <kazu@haystack.mit.edu>
2022-08-15 11:21:31 -0400

    add a test for get_solidangle

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using EHTUtils
 using Test
 
 @testset "EHTUtils.jl" begin
-    # Write your tests here.
     @test unitconv(milliarcsecond, as) == 0.001
     @test get_unit("rad") == rad
+    @test get_solidangle(1.0, 1.0, satype="b") == pi / (4 * log(2))
 end
