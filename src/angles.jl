e89e082d30be2518928ed413cf25fba435e01950
Kazunori Akiyama <kazu@haystack.mit.edu>
2022-08-15 11:12:21 -0400

    update arguments for get_solid angle

--- src/angles.jl
+++ src/angles.jl
@@ -2,14 +2,14 @@ export get_solidangle
 
 
 """
-    get_solidangle(x; [y=0, angunit=u"rad", angunitout=nothing, satype="pixel"])
+    get_solidangle(x, [y=0; angunit=u"rad", angunitout=nothing, satype="pixel"])
 
 compute the solid angle for a given set of the angular sizes at specified
 units of angular sizes in a specified type.
 
 
 # Arguments
-- `x, y::Number`: the size of the area in two orthogonal directions
+- `x, y::Number`: the size of the area in two orthogonal directions. if y <= 0, y = x.
 - `angunit::Unitful.Units or Unitful.Quantity`: the angular unit of `x` and `y`
 - `angunitout::Unitful.Units, Unitful.Quantity or nothing`: 
     the angular unit for the output solid angle. If nothing is specified,
@@ -17,11 +17,12 @@ units of angular sizes in a specified type.
 - `satype`: the type of the output solid angle:
     "pixel" for the solid angle of the rectangular area, and "beam" for the beam solid angle.
 """
-function get_solidangle(;
-    x=1, y=0,
+function get_solidangle(
+    x=1, y=-1;
     angunit=rad,
     angunitout=nothing,
-    satype::AbstractString="pixel")
+    satype::AbstractString="pixel"
+)
 
     # check x-value
     if x <= 0
