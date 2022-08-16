df0a4a2b851c8498cdb58920e7aa1dfe7bd8ca19
Kazunori Akiyama <kazu@haystack.mit.edu>
2022-08-16 10:55:36 -0400

    comment out rad2deg and deg2rad because of the existing standard functions

--- src/const.jl
+++ src/const.jl
@@ -2,21 +2,21 @@
 This file is setting up some constants
 to be used frequently in EHT data analysis
 """
-# Gaussian related
+# sigma, fwhm coversions for Gaussian
 const sigma2fwhm = sqrt(8.0 * log(2.0))::Float64
 const sigma2hwhm = sqrt(2.0 * log(2.0))::Float64
 const fwhm2sigma = 1.0 / sigma2fwhm::Float64
 const hwhm2sigma = 1.0 / sigma2hwhm::Float64
 
 # Angular Sizes
-const deg2rad = pi / 180.0::Float64
+#const deg2rad = pi / 180.0::Float64  (there is a standard function with the same name)
 const amin2rad = deg2rad / 60.0::Float64
 const asec2rad = deg2rad / 3600.0::Float64
 const mas2rad = asec2rad / 1e3::Float64
 const uas2rad = asec2rad / 1e6::Float64
 const μas2rad = uas2rad::Float64
 
-const rad2deg = 1.0 / deg2rad::Float64
+#const rad2deg = 1.0 / deg2rad::Float64  (there is a standard function with the same name)
 const rad2amin = 1.0 / amin2rad::Float64
 const rad2asec = 1.0 / asec2rad::Float64
 const rad2mas = 1.0 / mas2rad::Float64
