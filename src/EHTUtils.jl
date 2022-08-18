d3f03a6039c1728171b69a9487efe6b7e4af2aea
Kazunori Akiyama <kazu@haystack.mit.edu>
2022-08-18 14:20:48 -0400

    a minor change in the order of include

--- src/EHTUtils.jl
+++ src/EHTUtils.jl
@@ -4,6 +4,10 @@ using PhysicalConstants.CODATA2018: k_B, c_0  # for const.jl
 using Unitful
 using UnitfulAngles
 using UnitfulAstro
+using Logging
+
+# some quick tools for Logging
+include("logging.jl")
 
 # popular constants
 include("const.jl")
@@ -13,7 +17,4 @@ include("units.jl")
 
 # functions related to angles
 include("angles.jl")
-
-# some quick tools for Logging
-include("logging.jl")
 end
